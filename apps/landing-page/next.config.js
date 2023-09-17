20f137724c3f6f40a8367b3d959b0ab31b52382e
Christian Wijaya <christianwijaya.net@gmail.com>
2023-09-18 05:25:19 +0700

    Update next.config.js

--- apps/landing-page/next.config.js
+++ apps/landing-page/next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
   output: "standalone",
+  images: {
+    domains: ["images.unsplash.com", "cdn-icons-png.flaticon.com"],
+  },
 };
