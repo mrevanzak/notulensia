bd4b169253e64ac5adc9cd55c1f84a81d1cff527
Christian Wijaya <christianwijaya.net@gmail.com>
2023-09-18 06:01:11 +0700

    Add domain image

--- apps/landing-page/next.config.js
+++ apps/landing-page/next.config.js
@@ -3,7 +3,11 @@ module.exports = {
   transpilePackages: ["ui"],
   output: "standalone",
   images: {
-    domains: ["images.unsplash.com", "cdn-icons-png.flaticon.com"],
+    domains: [
+      "images.unsplash.com",
+      "cdn-icons-png.flaticon.com",
+      "c5.patreon.com",
+    ],
     formats: ["image/webp"],
   },
 };
