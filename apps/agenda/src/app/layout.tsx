183b15c581c89bdb6b6c71e8af32d6764831627b
mrevanzak <mrevanzak@gmail.com>
2023-10-19 13:28:52 +0700

    feat: tailwindcss

--- apps/agenda/src/app/layout.tsx
+++ apps/agenda/src/app/layout.tsx
@@ -1,14 +1,16 @@
 "use client";
-import {PrimeReactProvider} from "primereact/api";
+import { PrimeReactProvider } from "primereact/api";
 import "@/styles/layout/layout.scss";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/primereact.min.css";
 import "@/styles/demo/Demos.scss";
-import type {ReactElement} from "react";
-import {LayoutProvider} from "@/src/layout/context/layout-context";
+import "@/styles/globals.css";
+import type { ReactElement } from "react";
+import { LayoutProvider } from "@/src/layout/context/layout-context";
+import Tailwind from 'primereact/passthrough/tailwind';
 
-export default function RootLayout({children}): ReactElement {
+export default function RootLayout({ children }): ReactElement {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -19,7 +21,7 @@ export default function RootLayout({children}): ReactElement {
         />
       </head>
       <body>
-        <PrimeReactProvider>
+        <PrimeReactProvider value={{ pt: Tailwind }}>
           <LayoutProvider>{children}</LayoutProvider>
         </PrimeReactProvider>
       </body>
