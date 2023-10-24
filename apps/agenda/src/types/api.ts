d6112b75d3e042df117f8905639f87fa2871db66
root <root@ReV-PC>
2023-10-24 13:39:56 +0700

    refactor: some code improvements

--- apps/agenda/src/types/api.ts
+++ apps/agenda/src/types/api.ts
@@ -1,22 +1,4 @@
-import { AxiosError } from "axios";
-
-export class ApiError extends AxiosError {
-  constructor(error: ApiErrorResponse) {
-    super(error.message);
-    Object.setPrototypeOf(this, ApiError.prototype);
-  }
-
-  stringify(): string {
-    switch (this.message) {
-      case ErrorEnum.INVALID:
-        return "Email or password is invalid.";
-      default:
-        return "Something went wrong.";
-    }
-  }
-}
-
-type ApiErrorResponse = {
+export type ApiErrorResponse = {
   message: ErrorEnum;
 };
 
