e6286e21832f3d95c4a2e7d0d18f25092d5f4a06
mrevanzak <mrevanzak@gmail.com>
2023-11-06 18:20:57 +0700

    feat: remove unecessary files

--- apps/crm/src/components/sidebar/app-menu.tsx
+++ apps/crm/src/components/sidebar/app-menu.tsx
@@ -2,11 +2,6 @@ import type { ReactElement } from "react";
 import type { MenuModel } from "@/types/types";
 import AppSubMenu from "./app-sub-menu";
 
-import { TbHomeDot, TbListDetails } from "react-icons/tb";
-import { RxCalendar } from "react-icons/rx";
-import { MdOutlineSettings } from "react-icons/md";
-import { LuBadgeHelp } from "react-icons/lu";
-
 function AppMenu(): ReactElement {
   const model: MenuModel[] = [
     {
@@ -14,27 +9,22 @@ function AppMenu(): ReactElement {
       items: [
         {
           label: "Home",
-          icon: <TbHomeDot color="#4343BF" size={40} />,
           to: "/",
         },
         {
           label: "Events",
-          icon: <TbListDetails color="#4343BF" size={40} />,
           to: "/events",
         },
         {
           label: "Calendar",
-          icon: <RxCalendar color="#4343BF" size={40} />,
           to: "/calendar",
         },
         {
           label: "Settings",
-          icon: <MdOutlineSettings color="#4343BF" size={40} />,
           to: "/settings",
         },
         {
           label: "Help",
-          icon: <LuBadgeHelp color="#4343BF" size={40} />,
           to: "/help",
         },
       ],
