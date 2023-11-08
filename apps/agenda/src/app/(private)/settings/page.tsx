079a0cbddc127891daa1f46e805a489690fb92f6
mrevanzak <mrevanzak@gmail.com>
2023-11-08 15:13:12 +0700

    feat: setting page

--- apps/agenda/src/app/(private)/settings/page.tsx
+++ apps/agenda/src/app/(private)/settings/page.tsx
@@ -1,10 +1,23 @@
+import type { ReactElement } from "react";
 import React from "react";
+import Image from "next/image";
+import SettingForm from "@/components/forms/setting-form";
 
-export default function SettingsPage() {
+export default function SettingsPage(): ReactElement {
   return (
-    <div className="card bg-purple-50 tw-space-y-3 tw-min-h-[calc(100vh-4rem)]">
+    <div className="card bg-purple-50 tw-space-y-3 tw-min-h-[calc(100vh-4rem)] tw-flex tw-flex-col p-5">
       <h2>Settings</h2>
       <div className="tw-border-b-2 tw-border-dark-purple" />
+      <div className="tw-flex tw-flex-row tw-space-x-16 tw-flex-1">
+        <Image
+          alt="setting logo"
+          className="tw-aspect-square tw-h-full tw-my-auto"
+          height={490}
+          src="/img/setting.png"
+          width={490}
+        />
+        <SettingForm />
+      </div>
     </div>
   );
 }
---
+++ apps/agenda/src/components/forms/setting-form.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Button } from "primereact/button";
+import type { ReactElement } from "react";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import Checkbox from "../ui/checkbox";
+
+export default function SettingForm(): ReactElement {
+  // const { mutate, isPending, isError, error } = useSignIn();
+
+  const methods = useForm();
+  const { handleSubmit } = methods;
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+    // mutate({ ...data });
+  });
+
+  return (
+    <FormProvider {...methods}>
+      <form
+        className="tw-flex tw-flex-col tw-w-2/3 tw-space-y-8 tw-mt-8"
+        onSubmit={(event) => {
+          event.preventDefault();
+          void onSubmit();
+        }}
+      >
+        <div>
+          <h2 className="tw-font-normal">Notification Type</h2>
+          <p>Select you’re notifications type.</p>
+          <div className="tw-flex tw-space-x-8">
+            <Checkbox id="email" label="Email" />
+            <Checkbox id="telegram" label="Telegram" />
+            <Checkbox id="whatsapp" label="Whatsapp" />
+          </div>
+        </div>
+        <div>
+          <h2 className="tw-font-normal">Dashboard Priority</h2>
+          <p>Select you’re notifications type.</p>
+          <div className="tw-flex tw-space-x-8">
+            <Checkbox id="theme1" label="Theme 1" />
+            <Checkbox id="theme2" label="Theme 2" />
+            <Checkbox id="custom" label="Custom" />
+          </div>
+        </div>
+        <div className="tw-self-end tw-h-full tw-flex">
+          <Button className="tw-self-end" label="SAVE CHANGES" rounded />
+        </div>
+      </form>
+    </FormProvider>
+  );
+}
