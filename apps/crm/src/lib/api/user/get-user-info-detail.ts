c123fb9f74bceec6bd9921cba97fc57e58456483
farids13 <faridsatria24@gmail.com>
2023-12-19 20:08:55 +0700

    create profile in crm

--- apps/crm/src/lib/api/user/get-user-info-detail.ts
+++ apps/crm/src/lib/api/user/get-user-info-detail.ts
@@ -1,6 +1,6 @@
-import { httpClient } from "@/lib/http";
+import { httpClient, httpClientStorage } from "@/lib/http";
 import { profileSchema } from "@/lib/validations/user";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export const getUserInfoKey = "getUserInfo";
 
@@ -10,6 +10,23 @@ export const useGetUserInfo = () => {
     queryFn: async () => {
       const response = await httpClient.get("/user/me");
       return profileSchema.parse(response.data);
+    },
+  });
+};
+
+export const useDownloadUserImage = () => {
+  const { data: userData } = useGetUserInfo();
+
+  return useMutation({
+    mutationFn: async () => {
+      const imgUrl = userData?.imgUrl;
+      if (!userData || !imgUrl) {return new Blob()};
+
+      const response = await httpClientStorage.get(`storage/asset/${userData.imgUrl}`, {
+        responseType: "blob",
+      });
+
+      return response.data as Blob;
     },
   });
 };
--- apps/crm/src/lib/http.ts
+++ apps/crm/src/lib/http.ts
@@ -2,8 +2,11 @@ import { useAuthStore } from "@/stores/use-auth-store";
 import axios from "axios";
 import { ApiError } from "@/lib/error";
 
+export const API_URL = "https://agenda.saranaintegrasi.co.id/api/v1/crm";
+export const STORAGE_URL = "https://agenda.saranaintegrasi.co.id/api/v1";
+
 export const httpClient = axios.create({
-  baseURL: "https://agenda.saranaintegrasi.co.id/api/v1/crm",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
@@ -18,6 +21,37 @@ httpClient.interceptors.request.use((config) => {
 });
 
 httpClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response?.status === 401 ||
+      error.response?.data?.message === "UNAUTHORIZED"
+    ) {
+      useAuthStore.getState().logout();
+    }
+    if (error.response?.status === 400 || error.response?.status === 500) {
+      return Promise.reject(new ApiError(error.response?.data));
+    }
+    return Promise.reject(error);
+  },
+);
+
+export const httpClientStorage = axios.create({
+  baseURL: STORAGE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+httpClientStorage.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().access_token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+httpClientStorage.interceptors.response.use(
   (response) => response,
   (error) => {
     if (
