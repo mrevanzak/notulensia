eb6181f3bebcfc5ac955127b196e87764c20216b
farids13 <faridsatria24@gmail.com>
2024-01-10 12:30:01 +0700

    Fixing animation in Landing Page Without build

--- apps/agenda/src/app/(public)/page.tsx
+++ apps/agenda/src/app/(public)/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { Button } from "primereact/button";
 import type { ReactElement } from "react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import Logo from "~/svg/logo.svg";
 import LandingHero from "~/img/landing-hero.png";
 import HeroCreation from "~/img/hero-creation.png";
@@ -14,41 +14,64 @@ import BgHeroEvent from "~/img/bg-hero-event.png";
 import LogoFlat from "~/svg/logo-flat.svg";
 import { OverlayPanel } from "primereact/overlaypanel";
 import i18n from "../i18n";
-import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 
 export default function LandingPage(): ReactElement {
 
-  const controls1 = useAnimation();
-  const controls2 = useAnimation();
-  const controls3 = useAnimation();
   const [activeComponent, setActiveComponent] = useState(1);
-  useEffect(() => {
-    startAnim1();
-  }, [])
 
   const signIn = () => {
-    window.location.href = '/auth/signin';
+    window.location.href = '/auth/sign-in';
+
   }
 
-  const startAnim1 = () => {
-    void controls1.start({ opacity: 1, transition: { duration: 0.5 } });
-    setActiveComponent(1);
- 
-  }
-
-  const startAnim2 = () => {
-    void controls2.start({ opacity: 1, transition: { duration: 0.5 } });
-    setActiveComponent(2);
-  
-  }
-
-  const startAnim3 = () => {
-    void controls3.start({ opacity: 1, transition: { duration: 0.5 } });
-    setActiveComponent(3);
-  }
+  const handleComponentChange = (nextComponent) => {
+    setActiveComponent(nextComponent);
+  };
 
   const op = useRef<OverlayPanel>(null);
+
+  const pageHero1 = (
+    <div className="tw-flex tw-justify-between tw-pr-[54px] tw-bg-base-purple">
+      <Image alt="Hero Email" width={576} height={690} src={HeroEmail} />
+      <div className="tw-flex tw-flex-col tw-items-end tw-justify-center tw-space-y-8">
+        <h1 className="tw-align-top tw-text-[96px] tw-leading-normal tw-max-w-[844px] tw-text-right tw-text-white">Direct Email Invitations</h1>
+        <p className="tw-max-w-[958px] tw-text-right tw-text-[24px] tw-leading-normal tw-text-white">
+          Streamline the invitation process with direct email invitations sent to participants. Notulensia ensures that invitations reach your desired participants promptly and efficiently.
+        </p>
+        <Button className="tw-w-[285px] tw-h-[86px]" outlined onClick={signIn} label="Try It ->" style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "400", borderColor: "white", color: "white" }} />
+      </div>
+    </div>
+  );
+
+  const pageHero2 = (
+    <div className="tw-flex tw-flex-row-reverse tw-justify-between tw-gap-4">
+      <Image alt="Hero Invitation" width={576} height={690} src={HeroInvitation} />
+      <div className="tw-flex tw-flex-col tw-items-end tw-justify-center tw-space-y-8 ">
+        <h1 className="tw-align-top tw-text-[96px] tw-leading-normal tw-max-w-[896px] tw-text-right tw-text-base-purple">Invite Participants with Eases</h1>
+        <p className="tw-max-w-[938px] tw-text-right tw-text-[24px] tw-leading-normal tw-text-base-purple">
+          Include a feature for inviting participants, enabling you to send invitations quickly and effortlessly. Notulensia streamlines the invitation process, ensuring the presence of desired participants.
+        </p>
+        <Button className="tw-w-[285px] tw-h-[86px]" outlined onClick={signIn} label="Try It ->" style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "400", borderColor: "#334798", color: "#334798" }} />
+      </div>
+    </div>
+  );
+
+  const pageHero3 = (
+    <div className="tw-flex tw-justify-between tw-pr-[54px] tw-bg-base-pink">
+      <Image alt="Hero Creation" width={576} height={690} src={HeroCreation} />
+      <div className="tw-flex tw-flex-col tw-items-end tw-justify-center tw-space-y-8">
+        <h1 className="tw-align-top tw-text-[96px] tw-leading-normal tw-max-w-[844px] tw-text-right tw-text-white">Hassle-Free Meeting Creation</h1>
+        <p className="tw-max-w-[958px] tw-text-right tw-text-[24px] tw-leading-normal tw-text-white">
+          Introducing a feature that simplifies the meeting creation process. With Notulensia, setting up a meeting is quick and intuitive,
+          allowing you to focus on the substance of the discussion rather than the logistics.
+        </p>
+        <Button className="tw-w-[285px] tw-h-[86px]" outlined onClick={signIn} label="Try It ->" style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "400", borderColor: "white", color: "white" }} />
+      </div>
+    </div>
+  );
+
 
   const changeLanguage = (lang: string) => {
     void i18n.changeLanguage(lang);
@@ -58,51 +81,51 @@ export default function LandingPage(): ReactElement {
 
   return (
     <>
-    <div className="tw-font-exo tw-font-[600] tw-text-[20px] tw-w-full tw-h-[100px] tw-bg-white tw-flex tw-fixed tw-items-center tw-justify-between tw-px-10">
-      <div className="tw-flex tw-items-center">
-        <Logo className="tw-h-[57px] tw-w-[353px] tw-mx-32" />
-        <nav className="tw-flex tw-space-x-48">
-          <a href="#about">About</a>
-          <a href="#service">Service</a>
-          <a href="#contact">Contact</a>
-        </nav>
-      </div>
-      <div className="tw-flex tw-justify-center tw-gap-5">
-        <Button onClick={(e) => op.current?.toggle(e)} style={{border: 0, fontFamily: "Exo", color: 'black', fontWeight: 600, fontSize: "20px"}} outlined >En <i className="pi pi-angle-down tw-ml-2"/></Button>
-        <OverlayPanel ref={op}>
-          <div className="tw-flex tw-flex-col tw-mt-4 tw-space-y-4 tw-border-[#334798] tw-bg-red">
-              <Button onClick={() => {changeLanguage('en')}} outlined>
-                  <Image alt="Id Flag" height={20} src='/svg/flag/en.svg' width={20} />
-                  <span className="tw-ml-4">English</span>
+      <div className="tw-font-exo tw-font-[600] tw-text-[20px] tw-w-full tw-h-[100px] tw-bg-white tw-flex tw-fixed tw-items-center tw-justify-between tw-px-10">
+        <div className="tw-flex tw-items-center">
+          <Logo className="tw-h-[57px] tw-w-[353px] tw-mx-32" />
+          <nav className="tw-flex tw-space-x-48">
+            <a href="#about">About</a>
+            <a href="#service">Service</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </div>
+        <div className="tw-flex tw-justify-center tw-gap-5">
+          <Button onClick={(e) => op.current?.toggle(e)} style={{ border: 0, fontFamily: "Exo", color: 'black', fontWeight: 600, fontSize: "20px" }} outlined >En <i className="pi pi-angle-down tw-ml-2" /></Button>
+          <OverlayPanel ref={op}>
+            <div className="tw-flex tw-flex-col tw-mt-4 tw-space-y-4 tw-border-[#334798] tw-bg-red">
+              <Button onClick={() => { changeLanguage('en') }} outlined>
+                <Image alt="Id Flag" height={20} src='/svg/flag/en.svg' width={20} />
+                <span className="tw-ml-4">English</span>
               </Button>
 
-              <Button onClick={() => {changeLanguage('id')}} outlined>
+              <Button onClick={() => { changeLanguage('id') }} outlined>
                 <Image alt="Id Flag" height={20} src='/svg/flag/id.svg' width={20} />
                 <span className="tw-ml-4">Indonesia</span>
               </Button>
             </div>
           </OverlayPanel>
-        <Button label="Log In" onClick={() => {window.location.href = '/auth/sign-in'}} style={{ borderRadius: "1000px", backgroundColor: "#334798", width: "179px", height: "58px", fontFamily: "Exo", fontWeight: 600, fontSize: "20px" }} />
+          <Button label="Log In" onClick={() => { window.location.href = '/auth/sign-in' }} style={{ borderRadius: "1000px", backgroundColor: "#334798", width: "179px", height: "58px", fontFamily: "Exo", fontWeight: 600, fontSize: "20px" }} />
+        </div>
       </div>
-    </div>
 
 
-    <main className="tw-font-exo tw-w-full tw-bg-white tw-border-blue-400 tw-pt-[110px] tw-flex tw-flex-col tw-space-y-10">
+      <main className="tw-font-exo tw-w-full tw-bg-white tw-border-blue-400 tw-pt-[110px] tw-flex tw-flex-col tw-space-y-10">
         <article className="tw-flex tw-w-full tw-justify-center tw-items-start tw-pl-10 tw-pr-10">
           <div className="tw-w-[50%] tw-p-16">
             <div className="tw-mb-[20%] tw-max-w-[800px]">
               <h1 className="tw-text-base-purple tw-font-[600] tw-text-[85px] tw-w-full tw-leading-[100px]">Simplify Your Day, Organize Your Schedule</h1>
               <p className="tw-text-[30px] tw-text-red-500 tw-leading-relaxed">
-                This website streamlines your meeting scheduling,<br/>
-                making it easy<br/>
-                to plan meetings, invite participants,<br/>
-                and manage schedules efficiently.<br/>
+                This website streamlines your meeting scheduling,<br />
+                making it easy<br />
+                to plan meetings, invite participants,<br />
+                and manage schedules efficiently.<br />
               </p>
             </div>
-            <Button className="tw-w-[285px] tw-h-[86px]" label="Get Started" onClick={() => {window.location.href = '/auth/sign-in'}} style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "800", backgroundColor: "#334798", }}/>
+            <Button className="tw-w-[285px] tw-h-[86px]" label="Get Started" onClick={() => { window.location.href = '/auth/sign-in' }} style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "800", backgroundColor: "#334798", }} />
           </div>
           <Image className="tw-pt-10" width={835} height={778} src={LandingHero} alt="Hero" />
-        </article>    
+        </article>
 
         <article className="tw-flex tw-flex-col tw-text-xs tw-w-full tw-justify-center tw-items-center tw-space-y-5 tw-pl-10 tw-pr-10">
           <div className="tw-space-y-8">
@@ -111,57 +134,52 @@ export default function LandingPage(): ReactElement {
           </div>
 
           <div className="tw-w-[95%] tw-font-notosan ">
-            <ul className="tw-w-full tw-h-[69px] tw-grid tw-grid-cols-3 tw-text-[32px] tw-font-[600] tw-justify-center tw-items-center">
-              <li className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center tw-bg-base-purple tw-text-white" onMouseEnter={startAnim1}>Direct Email</li>
-              <li className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center" onMouseEnter={startAnim2} >Invite Participants</li>
-              <li className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center tw-bg-base-pink tw-text-white" onMouseEnter={startAnim3}>Create Meeting</li>
-            </ul>
+            <div className="tw-w-full tw-h-[69px] tw-grid tw-grid-cols-3 tw-text-[32px] tw-font-[600] tw-justify-center tw-items-center tw-relative">
+              <motion.div className={`tw-h-1 tw-w-60 tw-absolute  tw-rounded-full tw-bottom-1`}
+                initial={false}
+                animate={{ left: `calc(${activeComponent - 1} * (100% / 3) + (100% / 10.2))`, backgroundColor: activeComponent === 2 ? '#334798' : '#ffffff', }}
+                transition={{ duration: 0.8, type: 'spring', stiffness: 100, damping: 20, ease: 'easeIn' }}
+              />
+              <button className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center tw-bg-base-purple tw-text-white tw-outline-4 " onMouseEnter={() => setActiveComponent(1)} onClick={() => handleComponentChange(1)}>Direct Email</button>
+              <button className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center" onClick={() => handleComponentChange(2)} onMouseEnter={() => setActiveComponent(2)}>Invite Participants</button>
+              <button className="tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center tw-bg-base-pink tw-text-white" onClick={() => handleComponentChange(3)} onMouseEnter={() => setActiveComponent(3)}>Create Meeting</button>
+            </div>
 
             <div className="tw-h-[690px]">
-              <AnimatePresence>
+              <AnimatePresence mode="wait">
                 {activeComponent === 1 && (
-                  <motion.div initial={{ opacity: 0 }} animate={controls1}>
-                  <div className="tw-flex tw-justify-between tw-pr-[54px] tw-bg-base-purple">
-                      <Image alt="Hero Email" width={576} height={690} src={HeroEmail} />
-                      <div className="tw-flex tw-flex-col tw-items-end tw-justify-center tw-space-y-8">
-                        <h1 className="tw-align-top tw-text-[96px] tw-leading-normal tw-max-w-[844px] tw-text-right tw-text-white">Direct Email Invitations</h1>
-                        <p className="tw-max-w-[958px] tw-text-right tw-text-[24px] tw-leading-normal tw-text-white">
-                          Streamline the invitation process with direct email invitations sent to participants. Notulensia ensures that invitations reach your desired participants promptly and efficiently.
-                        </p>
-                        <Button className="tw-w-[285px] tw-h-[86px]" outlined onClick={signIn} label="Try It ->" style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "400", borderColor: "white", color: "white" }}/>
-                      </div>
-                    </div>
+                  <motion.div
+                    key="component-1"
+                    initial={{ x: "-100%" }}
+                    animate={{ x: 0 }}
+                    exit={{ x: "-100%" }}
+                    transition={{ duration: 2, ease: 'linear', x: { duration: 0.5 } }}
+                  >
+                    {pageHero1}
                   </motion.div>
                 )}
                 {activeComponent === 2 && (
-                  <motion.div initial={{ opacity: 0 }} animate={controls2}>
-                    <div className="tw-flex tw-flex-row-reverse tw-justify-between tw-gap-4">
-                      <Image alt="Hero Invitation" width={576} height={690} src={HeroInvitation} />
-                      <div className="tw-flex tw-flex-col tw-items-end tw-justify-center tw-space-y-8 ">
-                        <h1 className="tw-align-top tw-text-[96px] tw-leading-normal tw-max-w-[896px] tw-text-right tw-text-base-purple">Invite Participants with Eases</h1>
-                        <p className="tw-max-w-[938px] tw-text-right tw-text-[24px] tw-leading-normal tw-text-base-purple">
-                          Include a feature for inviting participants, enabling you to send invitations quickly and effortlessly. Notulensia streamlines the invitation process, ensuring the presence of desired participants.
-                        </p>
-                        <Button className="tw-w-[285px] tw-h-[86px]" outlined onClick={signIn} label="Try It ->" style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "400", borderColor: "#334798", color: "#334798" }}/>
-                      </div>
-                    </div>
+                  <motion.div
+                    key="component-2"
+                    initial={{ x: "100%" }}
+                    animate={{ x: 0 }}
+                    exit={{ x: "100%" }}
+                    transition={{ duration: 2, ease: 'linear', x: { duration: 0.5 } }}
+                  >
+                    {pageHero2}
                   </motion.div>
                 )}
                 {activeComponent === 3 && (
-                   <motion.div initial={{ opacity: 0 }} animate={controls3}>
-                    <div className="tw-flex tw-justify-between tw-pr-[54px] tw-bg-base-pink">
-                      <Image alt="Hero Creation" width={576} height={690} src={HeroCreation} />
-                      <div className="tw-flex tw-flex-col tw-items-end tw-justify-center tw-space-y-8">
-                        <h1 className="tw-align-top tw-text-[96px] tw-leading-normal tw-max-w-[844px] tw-text-right tw-text-white">Hassle-Free Meeting Creation</h1>
-                        <p className="tw-max-w-[958px] tw-text-right tw-text-[24px] tw-leading-normal tw-text-white">
-                          Introducing a feature that simplifies the meeting creation process. With Notulensia, setting up a meeting is quick and intuitive, 
-                          allowing you to focus on the substance of the discussion rather than the logistics.
-                        </p>
-                        <Button className="tw-w-[285px] tw-h-[86px]" outlined onClick={signIn} label="Try It ->" style={{ borderRadius: "1000px", fontSize: "25px", fontWeight: "400", borderColor: "white", color: "white" }}/>
-                      </div>
-                    </div>
+                  <motion.div
+                    key="component-3"
+                    initial={{ x: "-100%" }}
+                    animate={{ x: 0 }}
+                    exit={{ x: "-100%" }}
+                    transition={{ duration: 2, ease: 'linear', x: { duration: 0.5 } }}
+                  >
+                    {pageHero3}
                   </motion.div>
-                )}                
+                )}
               </AnimatePresence>
             </div>
           </div>
@@ -189,7 +207,7 @@ export default function LandingPage(): ReactElement {
 
         <footer className="tw-bg-base-purple tw-bg-opacity-50 tw-font-notosan">
           <div className="tw-flex tw-justify-center tw-items-center">
-            <LogoFlat className = "tw-w-[450px] tw-h-[73px] tw-pl-[129px]"/>
+            <LogoFlat className="tw-w-[450px] tw-h-[73px] tw-pl-[129px]" />
             <div className="tw-text-center tw-text-[32px] tw-py-10 tw-flex tw-flex-1 tw-justify-center tw-space-x-32">
               <a href="#">Company</a>
               <a href="#">Why Notulensia?</a>
@@ -201,7 +219,7 @@ export default function LandingPage(): ReactElement {
           <p className="tw-text-center tw-mt-5">&copy; 2024 PT Sarana Integrasi. All rights reserved.</p>
         </footer>
 
-    </main>
-  </>
+      </main>
+    </>
   );
 }
